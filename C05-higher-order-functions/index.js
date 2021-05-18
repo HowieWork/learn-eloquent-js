0d9b5d21900c887d594b655951a9cd7bddb8a6ba
Howie Hao Wang <howiewang2020@gmail.com>
2021-05-18 09:54:55 -0400

    Add text recognition function to index.js for chapter 5

--- C05-higher-order-functions/index.js
+++ C05-higher-order-functions/index.js
@@ -51,3 +51,59 @@ average(SCRIPTS.filter((script) => script.living).map((script) => script.year));
 average(
   SCRIPTS.filter((script) => !script.living).map((script) => script.year)
 );
+
+// 4.3 Recognizing text
+// textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"')
+
+// Step 1: Given a character code, find the corresponding script (if any)
+const characterScript = (code) => {
+  for (let script of SCRIPTS) {
+    if (
+      script.ranges.some(([from, to]) => {
+        return code >= from && code < to;
+      })
+    ) {
+      return script;
+    }
+  }
+  return null;
+};
+// console.log(characterScript(121));
+
+// Step 2: Take an array and a callback(test), return a new array of objects, which include NAME and COUNT properties (object format: {name: ..., count: ...})
+const countBy = (arr, test) => {
+  let newArr = [];
+  for (let item of arr) {
+    let name = test(item);
+    let known = newArr.findIndex((el) => el.name == name);
+    if (known == -1) {
+      newArr.push({ name, count: 1 });
+    } else {
+      newArr[known].count++;
+    }
+  }
+  return newArr;
+};
+// console.log(countBy([1, 2, 3, 4, 5], (n) => n > 2));
+// -> [{name: false, count: 2},{name:true, count:3}]
+
+// Step 3
+const textScripts = (text) => {
+  // Count the characters by name
+  let scripts = countBy(text, (char) => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.name : "none";
+  }).filter(({ name }) => name != "none");
+
+  // Calc total || NOthing found
+  let total = scripts.reduce((n, { count }) => n + count, 0);
+  if (total == 0) return "No scripts found";
+
+  return scripts
+    .map(({ name, count }) => {
+      return `${Math.round((count * 100) / total)}% ${name}`;
+    })
+    .join(", ");
+};
+
+// console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
